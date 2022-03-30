8df662da32882b1d34599c7ca648845bdb962f47
Nakzull <andr25f4@zbc.dk>
2022-03-30 09:23:27 +0200

    Add project files.

---
+++ GithubTest ABL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubTest_ABL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
